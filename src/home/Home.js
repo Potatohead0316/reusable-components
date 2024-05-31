d1c370d824fcfd0c6b3e852acf1c95b5bee009c7
Angelo S. Hagon <angelo.hagon@iloverbs.com>
2024-05-31 12:59:20 +0800

    update

--- src/home/Home.js
+++ src/home/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div className="form-container">
             <p className="title">Welcome to my page</p>
+            <h5 className="title">(Nothing to see yet)</h5>
         </div>
     );
 };
