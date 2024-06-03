b1fca04905a810aa28052bbb0ff5474d97be0941
Angelo S. Hagon <angelo.hagon@iloverbs.com>
2024-06-03 10:58:53 +0800

    update

--- src/home/Contacts.js
+++ src/home/Contacts.js
@@ -1,9 +1,36 @@
-
+import { Container, FormGroup, Input, InputGroup } from 'reactstrap';
 import './style.css';
 
 const Contacts = () => {
     return (
-        <>Contacts</>
+        <div>
+            <Container>
+                <FormGroup>
+                    <div className="rbs-label-required">Your Email</div>
+                    <InputGroup>
+                        <Input maxLength="250" id="bankAccountName" placeholder='Enter account name here...'
+                        // value={dataHelper.getValue(state.data, "bankAccountName", "string")} className="rbs-textbox-counter-left text-uppercase"
+                        />
+                    </InputGroup>
+                </FormGroup>
+                <FormGroup>
+                    <div className="rbs-label-required">Subject</div>
+                    <InputGroup>
+                        <Input maxLength="250" id="bankAccountName" placeholder='Enter account name here...'
+                        // value={dataHelper.getValue(state.data, "bankAccountName", "string")} className="rbs-textbox-counter-left text-uppercase"
+                        />
+                    </InputGroup>
+                </FormGroup>
+                <FormGroup>
+                    <div className="rbs-label-required">Message</div>
+                    <InputGroup>
+                        <Input maxLength="250" id="bankAccountName" placeholder='Enter account name here...'
+                        // value={dataHelper.getValue(state.data, "bankAccountName", "string")} className="rbs-textbox-counter-left text-uppercase"
+                        />
+                    </InputGroup>
+                </FormGroup>
+            </Container>
+        </div>
     );
 };
 
--- src/home/Intro.js
+++ src/home/Intro.js
@@ -7,7 +7,8 @@ const Intro = () => {
                 <div className='line1'>Hi, I'm Angelo </div>
                 <div className='line2'>A UI designer & React Developer </div>
                 <div className='line3'>Crafting Beautiful and Functional Designs. Welcome to my Portfolio. </div>
-                <button className='checkout-button'>CHECK OUT MY WORK</button>
+                <button className='checkout-button'>CONTACT ME</button>
+                <button className='contact-button'>CHECK OUT MY WORK</button>
             </div>
             <div className='image-content'>
                 <img src="images/images.png" alt="sample" />
